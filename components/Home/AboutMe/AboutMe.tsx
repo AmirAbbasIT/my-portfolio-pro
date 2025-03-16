dece2facd555cdcbe63fca46373f07af8d51e114
Amir Abbas <amirsaeed624@gmail.com>
2025-03-16 23:06:44 +0500

    updated profile pic

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -3,11 +3,21 @@ import Img from "../../../components/smallComp/image/Img";
 import ArrowIcon from "../../../components/Icons/ArrowIcon";
 export default function AboutMe(props) {
   const technologies = [
-    ["Nextjs/React", "TypeScript/JavaScript", "Tailwind CSS", "Ether.js/web3.js", "AWS/Google Cloud"],
+    [
+      "Nextjs/React",
+      "TypeScript/JavaScript",
+      "Tailwind CSS",
+      "Ether.js/web3.js",
+      "AWS/Google Cloud",
+    ],
     ["Node.js", "Java", "Solidity", "Mongodb", "GraphQL/RESTful APIs"],
   ];
   return (
-    <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col  items-center py-20 bg-AAprimary">
+    <div
+      id="aboutSection"
+      data-aos="fade-up"
+      className="snap-start flex flex-col  items-center py-20 bg-AAprimary"
+    >
       {/* // ? 0.1 About Me */}
       <div
         className="flex flex-col space-y-8 px-4 sm:px-0 w-full sm:w-[500px] 
@@ -15,8 +25,15 @@ export default function AboutMe(props) {
       >
         <div className="flex flex-row items-center ">
           <div className="flex flex-row  items-center mr-4">
-            <ArrowIcon className={"flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"} />
-            <span className="text-AAsecondary font-Header text-sm  sm:text-xl"> 01.</span>
+            <ArrowIcon
+              className={
+                "flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"
+              }
+            />
+            <span className="text-AAsecondary font-Header text-sm  sm:text-xl">
+              {" "}
+              01.
+            </span>
             <span className="flex-none text-gray-200 opacity-85 font-bold tracking-wider text-lg sm:text-2xl pl-4">
               About Me
             </span>
@@ -30,31 +47,45 @@ export default function AboutMe(props) {
             <div className="font-Header text-justify">
               <span className="text-gray-400 ">
                 Hello! My name is Abdellatif, and I&apos;m a passionate{" "}
-                <span className="text-AAsecondary">Full Stack Software Engineer</span> with strong problem-solving skills. My
-                journey in computer science started at 11 years old when I explored Ethical Hacking using BackTrack OS,
-                which started my interest in programming. Over the years, I continued to teach myself coding, which led
-                me to pursue an <span className="text-AAsecondary">engineering degree</span> and afterward a second
-                degree in <span className="text-AAsecondary">Master of Computer Science</span>, making me a professional
-                in this field.
+                <span className="text-AAsecondary">
+                  Full Stack Software Engineer
+                </span>{" "}
+                with strong problem-solving skills. My journey in computer
+                science started at 11 years old when I explored Ethical Hacking
+                using BackTrack OS, which started my interest in programming.
+                Over the years, I continued to teach myself coding, which led me
+                to pursue an{" "}
+                <span className="text-AAsecondary">engineering degree</span> and
+                afterward a second degree in{" "}
+                <span className="text-AAsecondary">
+                  Master of Computer Science
+                </span>
+                , making me a professional in this field.
               </span>
             </div>
             <div className="font-Header text-justify">
               <span className="text-gray-400 ">
-                Throughout my education and career, from my self-learning days to earning my degrees, I&apos;ve gained
-                experience in various industries. I have had the opportunity to work with companies and startups,
-                including <span className="text-AAsecondary">Vision</span>,{" "}
+                Throughout my education and career, from my self-learning days
+                to earning my degrees, I&apos;ve gained experience in various
+                industries. I have had the opportunity to work with companies
+                and startups, including{" "}
+                <span className="text-AAsecondary">Vision</span>,{" "}
                 <span className="text-AAsecondary">FeverTokens</span>,{" "}
-                <span className="text-AAsecondary">TrouveTavoi</span>, and many others, including transportation and
-                international companies. At FeverTokens, I led the development of an innovative NFT marketplace, and at
-                Vision, which is the <span className="text-AAsecondary">second-largest market</span> in the world for{" "}
-                <span className="text-AAsecondary">ENS</span>, I led the development of their new NameWrapper release.
+                <span className="text-AAsecondary">TrouveTavoi</span>, and many
+                others, including transportation and international companies. At
+                FeverTokens, I led the development of an innovative NFT
+                marketplace, and at Vision, which is the{" "}
+                <span className="text-AAsecondary">second-largest market</span>{" "}
+                in the world for <span className="text-AAsecondary">ENS</span>,
+                I led the development of their new NameWrapper release.
               </span>
             </div>
 
             <div className="font-Header tracking-wide text-justify ">
               <span className="text-gray-400  ">
-                I&apos;m always interested to keep learning and improve my skills to make a positive impact in the tech
-                world. Here are some of the technologies I&apos;ve been working with :{" "}
+                I&apos;m always interested to keep learning and improve my
+                skills to make a positive impact in the tech world. Here are
+                some of the technologies I&apos;ve been working with :{" "}
               </span>
             </div>
             <div className="font-Header tracking-wide flex flex-row space-x-16  justify-center lg:justify-start">
@@ -62,9 +93,14 @@ export default function AboutMe(props) {
                 <div className="flex flex-col space-y-4 sm:text-base text-sm">
                   {technologies[0].map((tech, index) => {
                     return (
-                      <div key={index} className="flex flex-row items-center space-x-2">
+                      <div
+                        key={index}
+                        className="flex flex-row items-center space-x-2"
+                      >
                         <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
-                        <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+                        <span className="text-gray-400 sm:text-sm text-xs">
+                          {tech}
+                        </span>
                       </div>
                     );
                   })}
@@ -74,9 +110,14 @@ export default function AboutMe(props) {
                 <div className="flex flex-col space-y-4 sm:text-base text-sm">
                   {technologies[1].map((tech, index) => {
                     return (
-                      <div key={index} className="flex flex-row items-center space-x-2">
+                      <div
+                        key={index}
+                        className="flex flex-row items-center space-x-2"
+                      >
                         <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
-                        <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+                        <span className="text-gray-400 sm:text-sm text-xs">
+                          {tech}
+                        </span>
                       </div>
                     );
                   })}
@@ -95,7 +136,7 @@ export default function AboutMe(props) {
             <div className="absolute w-5/6 h-5/6  rounded overflow-hidden">
               <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-300 rounded overflow-hidden"></div>
               <Img
-                src={"/img/Portfolio-portrait-3.jpg"}
+                src={"/img/my-pic.png"}
                 className={"object-contain rounded-lg"}
                 alt="My Image Not Found"
               />
